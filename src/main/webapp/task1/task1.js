9d8049de330ba8a61d0e3d85cfa4fc392cf2c912
Eirik Bjørsnøs <eirbjo@gmail.com>
2014-02-14 15:33:45 +0100

    Scope on controller

--- src/main/webapp/task1/task1.js
+++ src/main/webapp/task1/task1.js
@@ -1,49 +1,59 @@
 angular.module("CalcApp", [])
     .controller("CalcController",function ($scope) {
-        $scope.a = 100;
-        $scope.b = 100;
+        $scope.a = 180;
+        $scope.b = 80;
+        $scope.c = "c";
 
         $scope.operations = [
             {
                 name: "Sum",
                 operator: function (a, b) {
-                    return (parseInt(a) || 0) + (parseInt(b) || 0);
+                    return a + b;
                 }
             },
             {
                 name: "Difference",
                 operator: function (a, b) {
-                    return (parseInt(a) || 0) - (parseInt(b) || 0);
+                    return a-b
                 }
             },
             {
                 name: "Product",
                 operator: function (a, b) {
-                    return (parseInt(a) || 0) * (parseInt(b) || 0);
+                    return a * b;
                 }
             },
             {
                 name: "Quotient",
                 operator: function (a, b) {
-                    return (parseInt(a) || 0 / parseInt(b) || 0);
+                    if(b == 0) {
+                        return Infinity;
+                    }
+                    return a / b;
                 }
             },
             {
                 name: "Diagonal",
                 operator: function (a, b) {
-                    return Math.sqrt(Math.pow(parseInt(a), 2) + Math.pow(parseInt(b), 2));
+                    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
                 }
             }
         ];
 
 
     }).directive("calcSquare", function () {
-        console.log("calcSquare")
         return {
-            template: "<div class='boxlabel a' style='margin-left:{{width/2}}px'>a</div> <div class='boxlabel b' style='margin-top:{{height/2-7}}px'>b</div>",
+            restrict: 'E',
+            scope: {
+                a: "=a",
+                b: "=b"
+            },
+            template: "<div class='squarelabel a' style='margin-left:{{width/2}}px'>a:</div> <div class='squarelabel b' style='margin-top:{{height/2-7}}px'>b</div>",
+
             link: function (scope, elem, attrs) {
-                var maxWidth = parseInt(attrs.maxWidth) || 200;
-                var maxHeight = parseInt(attrs.maxHeight) || 200;
+
+                var maxWidth = parseInt(attrs.maxwidth) || 200;
+                var maxHeight = parseInt(attrs.maxheight) || 200;
 
                 function updateSize() {
                     var width = scope.a;
@@ -56,16 +66,43 @@ angular.module("CalcApp", [])
                         width = width * (maxHeight / height);
                         height = maxHeight;
                     }
+
                     scope.width = width;
                     scope.height = height;
+
 
                     elem.css("width", width + "px");
                     elem.css("height", height + "px");
 
                 }
 
-                scope.$watch(attrs.width, updateSize);
-                scope.$watch(attrs.height, updateSize);
+                scope.$watch(attrs.a, updateSize);
+                scope.$watch(attrs.b, updateSize);
             }
         }
     })
+    .directive("calcPositiveInteger", function() {
+        return {
+            require:"ngModel",
+            link: function(scope, elem, attrs, ngModel) {
+                ngModel.$parsers.unshift(function(value) {
+
+                    try {
+                        var num = parseInt(value);
+
+                        if(num < 0 || num.toString() !== value) {
+                            ngModel.$setValidity('positiveNumber', false);
+                            return undefined;
+                        } else {
+                            ngModel.$setValidity('positiveNumber', true);
+                            return num;
+                        }
+                    } catch (e) {
+                        ngModel.$setValidity('isNumber', false);
+                        return undefined;
+                    }
+                });
+            }
+        }
+    });
+
